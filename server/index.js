d8bc5703e7f65cd681f2b98da5f48d17d99350fc
hellociel <sweciel@gmail.com>
2021-03-03 21:56:17 -0800

    test

--- server/index.js
+++ server/index.js
@@ -7,28 +7,58 @@ const path = require('path');
 const app = express();
 // Set a constant for the port that our express server will listen on
 const PORT = 4000;
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-
+// const Todo = require('../database/db');
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
 // Start the server on the provided port
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
 
-const db = require('../database/db.js');
+// const db = require('../database/db.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json()) //difference?
 
 app.use(express.static(__dirname + '/../client/dist'));
+//
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+  console.log('connected to DB');
+});
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  time: String,
+  done: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
 
 app.post('/todo', function (req, res) {
+  // db.save(req, res, todo);
+
   let todo = { title: req.body.title, time: req.body.time, done: false };
-  db.save(req, res, todo);
+  let newTodo = new Todo(todo);
+  newTodo.save(todo, (err) => {
+    if (err) throw err;
+    res.send();
+  });
 });
 
 app.get('/todos', function (req, res) {
-  console.log('REQ', req);
-  db.get(req, res);
+  db.collection('todos').find({}, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 let port = 1128;
